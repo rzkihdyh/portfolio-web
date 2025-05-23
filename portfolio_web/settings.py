094a07c97f9ad8ae446d9adc867c47d394271f7e
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-05-23 19:58:08 +0700

    ready to vercell 2

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7k1i%-7tu7o2h2bg*mp87)aqnlju!@zck)x64!=-kb8p-ss=v4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rizki-hidayatul-profile.vercel.app']
 
 
 # Application definition
