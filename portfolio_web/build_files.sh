e63cb632081da9bbe9df8cfc40086fd59dd0fbfe
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-05-23 21:13:28 +0700

    semoga ke load

--- portfolio_web/build_files.sh
+++ portfolio_web/build_files.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3.13 -m pip install -r requirements.txt
+echo "📦 Installing dependencies..."
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 
-echo "Make Migration..."
-python3.13 manage.py makemigrations --noinput
-python3.13 manage.py migrate --noinput
+echo "🛠️ Running database migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.13 manage.py collectstatic --noinput --clear
-
-# install dependencies
-pip install -r requirements.txt
-
-# collect static files
-python3 manage.py collectstatic --noinput
-
-# copy hasil collectstatic ke staticfiles_build
-mkdir -p staticfiles_build
-cp -r staticfiles/* staticfiles_build/
+echo "🎨 Collecting static files..."
+python3.9 manage.py collectstatic --noinput
