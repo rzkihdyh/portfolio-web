8601ca947add3bdc9e20fe9aa7edcd75b8c393cc
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-05-23 20:49:03 +0700

    nambahin css asli bnget banggg

--- portfolio_web/build_files.sh
+++ portfolio_web/build_files.sh
@@ -2,14 +2,14 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.13 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.13 manage.py makemigrations --noinput
+python3.13 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.13 manage.py collectstatic --noinput --clear
 
 # install dependencies
 pip install -r requirements.txt
