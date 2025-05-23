1841c01c06ce8931587da55343dd43f03f546932
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-05-23 21:11:26 +0700

    BISMILLAH KE LOAD

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # # Static files configuration untuk production
 # if not DEBUG:
