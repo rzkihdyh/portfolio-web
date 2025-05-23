875f01c928add0f811ba1e5b1cf9b43f3c6c6603
rizki.hidayatul <rizki.hidayatul@ui.ac.id>
2025-05-23 19:45:29 +0700

    switch debug

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7k1i%-7tu7o2h2bg*mp87)aqnlju!@zck)x64!=-kb8p-ss=v4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
